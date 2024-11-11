b3353160da38f0156bcff7f2a01057b87bd09bbc
Harish-123456787 <124224081+Harish-123456787@users.noreply.github.com>
2024-11-11 10:48:31 +0530

    Add files via upload

---
+++ exp 19(Implementation of Naive Bayes in Python  Machine Learning).py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+data = load_iris()
+X = data.data  # Features
+y = data.target  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+model = GaussianNB()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy * 100:.2f}%')
